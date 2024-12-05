2340f62df15b81fd92c7de564d50ce89f6909922
Discipuls <87646124+Discipuls@users.noreply.github.com>
2024-12-05 12:39:23 +0300

    Initial

---
+++ Algorithms.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Laba4
+{
+    public static class Algorithms
+    {
+        public static void StepAlgorithm(Canvas canvas, int x1, int y1, int x2, int y2, TextBlock outputTextBlock)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+            double xIncrement = dx / (double)steps;
+            double yIncrement = dy / (double)steps;
+            double x = x1;
+            double y = y1;
+            outputTextBlock.Text += "";
+
+
+            for (int i = 0; i <= steps; i++)
+            {
+                var point = new Ellipse
+                {
+                    Width = 5,
+                    Height = 5,
+                    Fill = Brushes.Red
+                };
+                Canvas.SetLeft(point, x);
+                Canvas.SetTop(point, y);
+                canvas.Children.Add(point);
+
+
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+
+        public static void CDAAlgorithm(Canvas canvas, int x1, int y1, int x2, int y2, TextBlock outputTextBlock)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+            double x = x1;
+            double y = y1;
+            double xIncrement = dx / (double)steps;
+            double yIncrement = dy / (double)steps;
+
+            outputTextBlock.Text += $"dx = {dx}, dy = {dy}, steps = {steps}\n";
+            outputTextBlock.Text += "Шаги: (1 из 100)\n";
+
+            for (int i = 0; i <= steps; i++)
+            {
+                var point = new Ellipse
+                {
+                    Width = 5,
+                    Height = 5,
+                    Fill = Brushes.Green
+                };
+                Canvas.SetLeft(point, x);
+                Canvas.SetTop(point, y);
+                canvas.Children.Add(point);
+                if (i % 50 == 0)
+                {
+                    outputTextBlock.Text += $"Точка: ({(int)Math.Round(x)}, {(int)Math.Round(y)})\n";
+
+                }
+
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+
+        public static void BresenhamAlgorithm(Canvas canvas, int x1, int y1, int x2, int y2, TextBlock outputTextBlock)
+        {
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+            int sx = (x1 < x2) ? 1 : -1;
+            int sy = (y1 < y2) ? 1 : -1;
+            int err = dx - dy;
+
+            outputTextBlock.Text += "";
+
+            while (true)
+            {
+                var point = new Ellipse
+                {
+                    Width = 5,
+                    Height = 5,
+                    Fill = Brushes.Blue
+                };
+                Canvas.SetLeft(point, x1);
+                Canvas.SetTop(point, y1);
+                canvas.Children.Add(point);
+
+
+                if (x1 == x2 && y1 == y2) break;
+
+                int e2 = err * 2;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x1 += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+        }
+
+        public static void BresenhamCircle(Canvas canvas, int xc, int yc, int r, TextBlock outputTextBlock)
+        {
+            int x = 0;
+            int y = r;
+            int d = 3 - 2 * r;
+
+            outputTextBlock.Text += "";
+
+
+            while (x <= y)
+            {
+                DrawCirclePoints(canvas, xc, yc, x, y, outputTextBlock);
+                if (d < 0)
+                {
+                    d = d + 4 * x + 6;
+                }
+                else
+                {
+                    d = d + 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+
+            }
+        }
+
+        private static void DrawCirclePoints(Canvas canvas, int xc, int yc, int x, int y, TextBlock outputTextBlock)
+        {
+            DrawPoint(canvas, xc + x, yc + y, outputTextBlock);
+            DrawPoint(canvas, xc - x, yc + y, outputTextBlock);
+            DrawPoint(canvas, xc + x, yc - y, outputTextBlock);
+            DrawPoint(canvas, xc - x, yc - y, outputTextBlock);
+            DrawPoint(canvas, xc + y, yc + x, outputTextBlock);
+            DrawPoint(canvas, xc - y, yc + x, outputTextBlock);
+            DrawPoint(canvas, xc + y, yc - x, outputTextBlock);
+            DrawPoint(canvas, xc - y, yc - x, outputTextBlock);
+        }
+
+        private static void DrawPoint(Canvas canvas, int x, int y, TextBlock outputTextBlock)
+        {
+            var point = new Ellipse
+            {
+                Width = 5,
+                Height = 5,
+                Fill = Brushes.Purple
+            };
+            Canvas.SetLeft(point, x);
+            Canvas.SetTop(point, y);
+            canvas.Children.Add(point);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Laba4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RedrawCanvas();
+        }
+
+        private void OnStepAlgorithmClick(object sender, RoutedEventArgs e)
+        {
+            DrawStepAlgorithm();
+        }
+
+        private void OnCDAAlgorithmClick(object sender, RoutedEventArgs e)
+        {
+            DrawCDAAlgorithm();
+        }
+
+        private void OnBresenhamAlgorithmClick(object sender, RoutedEventArgs e)
+        {
+            DrawBresenhamAlgorithm();
+        }
+
+        private void OnBresenhamCircleClick(object sender, RoutedEventArgs e)
+        {
+            DrawBresenhamCircle();
+        }
+
+        private void RedrawCanvas()
+        {
+            DrawingCanvas.Children.Clear();
+            DrawAxes();
+            DrawGrid();
+        }
+
+        private void DrawAxes()
+        {
+            DrawingCanvas.Children.Add(new Line
+            {
+                X1 = 400,
+                Y1 = 0,
+                X2 = 400,
+                Y2 = 600,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            });
+
+            DrawingCanvas.Children.Add(new Line
+            {
+                X1 = 0,
+                Y1 = 300,
+                X2 = 800,
+                Y2 = 300,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            });
+
+            DrawingCanvas.Children.Add(new TextBlock
+            {
+                Text = "Ось X",
+                FontSize = 16,
+                Foreground = Brushes.Black,
+                Margin = new Thickness(410, 10, 0, 0)
+            });
+
+            DrawingCanvas.Children.Add(new TextBlock
+            {
+                Text = "Ось Y",
+                FontSize = 16,
+                Foreground = Brushes.Black,
+                Margin = new Thickness(10, 290, 0, 0)
+            });
+        }
+
+        private void DrawGrid()
+        {
+            for (int i = -30; i <= 30; i++)
+            {
+                DrawingCanvas.Children.Add(new Line
+                {
+                    X1 = (i * 10 + 400),
+                    Y1 = 0,
+                    X2 = (i * 10 + 400),
+                    Y2 = 600,
+                    Stroke = Brushes.LightGray,
+                    StrokeThickness = 0.5
+                });
+
+                DrawingCanvas.Children.Add(new Line
+                {
+                    X1 = 0,
+                    Y1 = (i * 10 + 300),
+                    X2 = 800,
+                    Y2 = (i * 10 + 300),
+                    Stroke = Brushes.LightGray,
+                    StrokeThickness = 0.5
+                });
+
+                if (i != 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        DrawingCanvas.Children.Add(new TextBlock
+                        {
+                            Text = (i * 10).ToString(),
+                            FontSize = 12,
+                            Foreground = Brushes.Black,
+                            Margin = new Thickness((i * 10 + 400) + 5, 290, 0, 0)
+                        });
+                        DrawingCanvas.Children.Add(new TextBlock
+                        {
+                            Text = (i * 10).ToString(),
+                            FontSize = 12,
+                            Foreground = Brushes.Black,
+                            Margin = new Thickness(10, (i * 10 + 300) - 8, 0, 0)
+                        });
+                    }
+
+
+                  
+                }
+            }
+        }
+
+        private void ClearCanvas()
+        {
+            DrawingCanvas.Children.Clear();
+            RedrawCanvas();
+        }
+
+
+        private void DrawStepAlgorithm()
+        {
+            ClearCanvas();
+            OutputTextBlock.Text = "Алгоритм Пошаговой растеризации (вычисления):\n";
+
+            int x1 = 100, y1 = 100, x2 = 700, y2 = 500;
+            Algorithms.StepAlgorithm(DrawingCanvas, x1, y1, x2, y2, OutputTextBlock);
+        }
+
+        private void DrawCDAAlgorithm()
+        {
+            ClearCanvas();
+            OutputTextBlock.Text = "Алгоритм ЦДА (вычисления):\n";
+
+            int x1 = 100, y1 = 100, x2 = 700, y2 = 500;
+            Algorithms.CDAAlgorithm(DrawingCanvas, x1, y1, x2, y2, OutputTextBlock);
+        }
+
+        private void DrawBresenhamAlgorithm()
+        {
+            ClearCanvas();
+            OutputTextBlock.Text = "Алгоритм Брезенхема (вычисления):\n";
+
+            int x1 = 100, y1 = 100, x2 = 700, y2 = 500;
+            Algorithms.BresenhamAlgorithm(DrawingCanvas, x1, y1, x2, y2, OutputTextBlock);
+        }
+
+        private void DrawBresenhamCircle()
+        {
+            ClearCanvas();
+            OutputTextBlock.Text = "Алгоритм Брезенхема (окружность) (вычисления):\n";
+
+            int xc = 400, yc = 300, r = 200;
+            Algorithms.BresenhamCircle(DrawingCanvas, xc, yc, r, OutputTextBlock);
+        }
+    }
+}
